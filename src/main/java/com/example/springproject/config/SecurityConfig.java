66d850dc6436b9f944bdf5588998bb71b5da77ad
Kamil Pula <kamil.pula.dev@gmail.com>
2023-11-06 12:19:18 +0100

    Moved config

--- src/main/java/com/example/springproject/config/SecurityConfig.java
+++ src/main/java/com/example/springproject/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.springproject.security;
+package com.example.springproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/springproject/controller/AuthController.java
+++ src/main/java/com/example/springproject/controller/AuthController.java
@@ -1,5 +1,12 @@
 package com.example.springproject.controller;
 
+import com.example.springproject.dto.LoginDTO;
+import com.example.springproject.dto.RegisterDTO;
+import com.example.springproject.model.Role;
+import com.example.springproject.model.User;
+import com.example.springproject.repository.RoleRepository;
+import com.example.springproject.repository.UserRepository;
+import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,66 +20,66 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springproject.dto.LoginDTO;
-import com.example.springproject.dto.RegisterDTO;
-import com.example.springproject.model.Role;
-import com.example.springproject.model.User;
-import com.example.springproject.repository.RoleRepository;
-import com.example.springproject.repository.UserRepository;
-
-import java.util.Collections;
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+  @Autowired
+  private AuthenticationManager authenticationManager;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+  @Autowired
+  private RoleRepository roleRepository;
 
-    @Autowired
-    private RoleRepository roleRepository;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+  @PostMapping("/login")
+  public ResponseEntity<String> authenticateUser(
+    @RequestBody LoginDTO loginDto
+  ) {
+    Authentication authentication = authenticationManager.authenticate(
+      new UsernamePasswordAuthenticationToken(
+        loginDto.getUsernameOrEmail(),
+        loginDto.getPassword()
+      )
+    );
 
-    @PostMapping("/login")
-    public ResponseEntity<String> authenticateUser(@RequestBody LoginDTO loginDto){
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+  }
 
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+  @PostMapping("/register")
+  public ResponseEntity<?> registerUser(@RequestBody RegisterDTO registerDTO) {
+    // add check for username exists in a DB
+    if (userRepository.existsByUsername(registerDTO.getUsername())) {
+      return new ResponseEntity<>(
+        "Username is already taken!",
+        HttpStatus.BAD_REQUEST
+      );
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterDTO registerDTO){
-
-        // add check for username exists in a DB
-        if(userRepository.existsByUsername(registerDTO.getUsername())){
-            return new ResponseEntity<>("Username is already taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        // add check for email exists in DB
-        if(userRepository.existsByEmail(registerDTO.getEmail())){
-            return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
-        }
-
-        // create user object
-        User user = new User();
-        user.setName(registerDTO.getName());
-        user.setUsername(registerDTO.getUsername());
-        user.setEmail(registerDTO.getEmail());
-        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
-
-        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
-        user.setRoles(Collections.singleton(roles));
-
-        userRepository.save(user);
-
-        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
-
+    // add check for email exists in DB
+    if (userRepository.existsByEmail(registerDTO.getEmail())) {
+      return new ResponseEntity<>(
+        "Email is already taken!",
+        HttpStatus.BAD_REQUEST
+      );
     }
+
+    // create user object
+    User user = new User();
+    user.setName(registerDTO.getName());
+    user.setUsername(registerDTO.getUsername());
+    user.setEmail(registerDTO.getEmail());
+    user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+    Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+    user.setRoles(Collections.singleton(roles));
+
+    userRepository.save(user);
+
+    return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+  }
 }
