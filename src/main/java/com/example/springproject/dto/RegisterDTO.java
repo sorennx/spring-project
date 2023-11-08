7963b164ca0830e5ebf95f479627e71fd3a8972f
Kamil Pula <kamil.pula.dev@gmail.com>
2023-11-08 09:54:45 +0100

    Updated logic

--- src/main/java/com/example/springproject/dto/RegisterDTO.java
+++ src/main/java/com/example/springproject/dto/RegisterDTO.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 @Data
 public class RegisterDTO {
-    private String name;
-    private String username;
-    private String email;
-    private String password;
+  private String email;
+  private String password;
 }
-
